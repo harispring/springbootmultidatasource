60bef989dc2ce0d8376169e3875273831b3603c0
harispring <hari.spring4s@gmail.com>
2025-03-21 21:19:10 +0530

    code commit successfull multidatasource

---
+++ src/main/java/com/kb/sample/multidatasourcedemo/spring_boot_tutorials1/SpringBootTutorials1Application.java
@@ -0,0 +1,13 @@
+package com.kb.sample.multidatasourcedemo.spring_boot_tutorials1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorials1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorials1Application.class, args);
+	}
+
+}
